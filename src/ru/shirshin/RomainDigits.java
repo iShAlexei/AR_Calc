8e2c2ab19eeb3ecd54e033c79dd389e43fd6dd65
Alexey Shirshin <110342036+iShAlexei@users.noreply.github.com>
2023-10-31 12:24:03 +0000

    commit #004 add: enum RomainDigits

---
+++ src/ru/shirshin/RomainDigits.java
@@ -0,0 +1,26 @@
+package ru.shirshin;
+
+enum RomainDigits {
+
+    I("I",1), 
+    V("V", 5), 
+    X("X", 10), 
+    L("L", 50), 
+    C("C", 100);
+
+    private String romain;
+    private int arabian;
+
+    RomainDigits(String romain, int arabian) {
+        this.romain = romain;
+        this.arabian = arabian;
+    }
+
+    public String getRomain() {
+        return romain;
+    }
+
+    public int getArabian() {
+        return arabian;
+    }
+}
